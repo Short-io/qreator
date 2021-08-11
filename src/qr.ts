3a9dd267c06a5dd4015148e043a8dbda374d8ef7
Igor Rybak <igor@short.io>
2021-08-11 13:08:49 +0300

    rename function

--- src/qr.ts
+++ src/qr.ts
@@ -3,7 +3,7 @@ import { getPNG } from "./png";
 import { getSVG } from "./svg";
 import { ImageOptions } from "./typing/types";
 
-export async function qrImage(text: string, options: ImageOptions) {
+export async function getQRImage(text: string, options: ImageOptions) {
     switch (options.type) {
         case "svg":
             return getSVG(text, options);
