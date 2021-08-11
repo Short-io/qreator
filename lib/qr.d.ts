38e6416cc13f1c5f8e18c627d8de79ac1c093271
Igor Rybak <igor@short.io>
2021-08-11 15:14:48 +0300

    update image options type

--- lib/qr.d.ts
+++ lib/qr.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="node" />
 import { ImageOptions } from "./typing/types";
-export declare function getQRImage(text: string, options: ImageOptions): Promise<Uint8Array | Buffer>;
+export declare function getQRImage(text: string, options: Omit<ImageOptions, "type"> & Required<Pick<ImageOptions, "type">>): Promise<Uint8Array | Buffer>;
