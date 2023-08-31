e7c2efee561467f5988c9374f121273ba3ff8db8
Andrii Kostenko <andrey@kostenko.name>
2023-08-31 18:03:51 +0000

    upgrade deps

--- src/qr.ts
+++ src/qr.ts
@@ -1,16 +1,2 @@
 import { ImageOptions } from "./typing/types";
 export type QRImageOptions = Omit<ImageOptions, "type"> & Required<Pick<ImageOptions, "type">>;
-
-/**
- * @deprecated
- * 
- * @param text text to encode
- * @param options options
- * @returns Buffer of the image
- */
-export async function getQRImage(
-    text: string,
-    options: QRImageOptions
-) {
-    throw new Error("getQRImage is deprecated. Use getPNG, getSVG or getPDF instead. If will save your memory and bundle size.");
-}
